5337f139becbfa2a3f2e1fc8c9aa2476538972f3
NT.Dang.20 <73978541+githubnguyendang@users.noreply.github.com>
2024-04-26 15:08:59 +0700

    Update BieuMauDto.cs

--- new-wr-api/Dto/BieuMauDto.cs
+++ new-wr-api/Dto/BieuMauDto.cs
@@ -171,14 +171,13 @@
     public class BieuMauMuoiHaiDto
     {
         public int? Id { get; set; }
-        public string? LuuVucSong { get; set; }
-        public double? TongSoCongTrinh { get; set; }
+        public string? TenLVS { get; set; }
+        public double? TongCongTrinh { get; set; }
         public double? TuoiNguonNuocMat { get; set; }
         public double? TuoiNguonNuocDuoiDat { get; set; }
         public double? KhaiThacThuyDien { get; set; }
         public double? MucDichKhacNguonNuocMat { get; set; }
         public double? MucDichKhacNguonNuocDD { get; set; }
-        public string? GhiChu { get; set; }
     }
 
     public class BieuMauMuoiBaDto
