6dffd6a26ccc4cdd67e422acb7ecca70921d9c86
NT.Dang.20 <73978541+githubnguyendang@users.noreply.github.com>
2024-05-06 16:47:10 +0700

    Update BieuMauMuoiMotService.cs

--- new-wr-api/Service/BaoCaoBieuMau/BieuMauMuoiMotService.cs
+++ new-wr-api/Service/BaoCaoBieuMau/BieuMauMuoiMotService.cs
@@ -19,7 +19,7 @@ namespace new_wr_api.Service
         }
         public async Task<List<BieuMauMuoiMotDto>> GetAllAsync()
         {
-            var ids = new HashSet<int?> { 5, 11, 12, 6, 14, 7, 15 };
+            var ids = new HashSet<int?> { 5, 10, 11, 12, 6, 14, 7, 15 };
 
             var items = await _context.LuuVucSong!
                  .Where(lvs => lvs.Id > 0)
