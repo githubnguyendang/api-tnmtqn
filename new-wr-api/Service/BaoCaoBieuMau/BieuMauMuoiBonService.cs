5fca86ef91beac3c748fed16ee32f6cf784cad4d
trungquan21 <89087554+trungquan21@users.noreply.github.com>
2024-05-07 15:35:08 +0700

    #fix bieu mau 14

--- new-wr-api/Service/BaoCaoBieuMau/BieuMauMuoiBonService.cs
+++ new-wr-api/Service/BaoCaoBieuMau/BieuMauMuoiBonService.cs
@@ -16,7 +16,7 @@ namespace new_wr_api.Service
             _mapper = mapper;
         }
 
-        public async Task<List<BieuMauMuoiBonDto>> GetAllAsync(int? nam)
+        public async Task<List<BieuMauMuoiBonDto>?> GetAllAsync(int? nam)
         {
             var items = _context.CLN_NDD!.Where(d => d.DaXoa == false && d.ThoiGianQuanTrac == nam)
                 .Select(cln => new BieuMauMuoiBonDto
@@ -26,30 +26,30 @@ namespace new_wr_api.Service
                     LuuVucSong = cln.LuuVucSong,
                     TangChuaNuoc = cln.TangChuaNuoc,
                     ViTriQuanTrac = cln.ViTriQuanTrac,
-                    PHlonNhat = new[] { cln.PhDot1 ?? 0, cln.PhDot2 ?? 0, cln.PhDot3 ?? 0 }.Max(),
-                    PHNhoNhat = new[] { cln.PhDot1 ?? 0, cln.PhDot2 ?? 0, cln.PhDot3 ?? 0 }.Min(),
-                    PHTrungBinh = Math.Round((double)new[] { cln.PhDot1 ?? 0, cln.PhDot2 ?? 0, cln.PhDot3 ?? 0 }.Average()!, 2),
-                    ColiformlonNhat = new[] { cln.ColiformDot1 ?? 0, cln.ColiformDot2 ?? 0, cln.ColiformDot3 ?? 0 }.Max(),
-                    ColiformNhoNhat = new[] {cln.ColiformDot1 ?? 0, cln.ColiformDot2 ?? 0, cln.ColiformDot3 ?? 0 }.Min(),
-                    ColiformTrungBinh = Math.Round((double)new[] {cln.ColiformDot1 ?? 0, cln.ColiformDot2 ?? 0, cln.ColiformDot3 ?? 0 }.Average()!, 2),
-                    NitratelonNhat = new[] {cln.NitrateDot1 ?? 0, cln.NitrateDot2 ?? 0, cln.NitrateDot3 ?? 0 }.Max(),
-                    NitrateNhoNhat = new[] {cln.NitrateDot1 ?? 0, cln.NitrateDot2 ?? 0, cln.NitrateDot3 ?? 0 }.Min(),
-                    NitrateTrungBinh = Math.Round((double)new[] { cln.NitrateDot1 ?? 0, cln.NitrateDot2 ?? 0, cln.NitrateDot3 ?? 0 }.Average()!, 2),
-                    AmonilonNhat = new[] { cln.AmoniDot1 ?? 0, cln.AmoniDot2 ?? 0, cln.AmoniDot3 ?? 0 }.Max(),
-                    AmoniNhoNhat = new[] { cln.AmoniDot1 ?? 0, cln.AmoniDot2 ?? 0 , cln.AmoniDot3 ?? 0 }.Min(),
-                    AmoniTrungBinh = Math.Round((double)new[] { cln.AmoniDot1 ?? 0, cln.AmoniDot2 ?? 0, cln.AmoniDot3 ?? 0 }.Average()!, 2),
-                    TDSlonNhat = new[] { cln.TDSDot1 ?? 0, cln.TDSDot2 ?? 0, cln.TDSDot3 ?? 0 }.Max(),
-                    TDSNhoNhat = new[] { cln.TDSDot1 ?? 0, cln.TDSDot2 ?? 0, cln.TDSDot3 ?? 0 }.Min(),
-                    TDSTrungBinh = Math.Round((double)new[] { cln.TDSDot1 ?? 0, cln.TDSDot2 ?? 0, cln.TDSDot3 ?? 0 }.Average()!, 2),
-                    DoCungLonNhat = new[] { cln.DoCungDot1 ?? 0, cln.DoCungDot2 ?? 0, cln.DoCungDot3 ?? 0 }.Max(),
+                    PHlonNhat = new[] { cln.PhDot1 , cln.PhDot2 , cln.PhDot3  }.Max(),
+                    PHNhoNhat = new[] { cln.PhDot1, cln.PhDot2, cln.PhDot3 }.Min(),
+                    PHTrungBinh = Math.Round((double)new[] { cln.PhDot1 , cln.PhDot2 , cln.PhDot3  }.Average()!, 2),
+                    ColiformlonNhat = new[] { cln.ColiformDot1 , cln.ColiformDot2 , cln.ColiformDot3  }.Max(),
+                    ColiformNhoNhat = new[] { cln.ColiformDot1 , cln.ColiformDot2 , cln.ColiformDot3  }.Min(),
+                    ColiformTrungBinh = Math.Round((double)new[] { cln.ColiformDot1 , cln.ColiformDot2 , cln.ColiformDot3  }.Average()!, 2),
+                    NitratelonNhat = new[] { cln.NitrateDot1 , cln.NitrateDot2 , cln.NitrateDot3  }.Max(),
+                    NitrateNhoNhat = new[] { cln.NitrateDot1 , cln.NitrateDot2 , cln.NitrateDot3  }.Min(),
+                    NitrateTrungBinh = Math.Round((double)new[] { cln.NitrateDot1 , cln.NitrateDot2 , cln.NitrateDot3  }.Average()!, 2),
+                    AmonilonNhat = new[] { cln.AmoniDot1 , cln.AmoniDot2 , cln.AmoniDot3  }.Max(),
+                    AmoniNhoNhat = new[] { cln.AmoniDot1 , cln.AmoniDot2 , cln.AmoniDot3  }.Min(),
+                    AmoniTrungBinh = Math.Round((double)new[] { cln.AmoniDot1 , cln.AmoniDot2 , cln.AmoniDot3  }.Average()!, 2),
+                    TDSlonNhat = new[] { cln.TDSDot1 , cln.TDSDot2 , cln.TDSDot3  }.Max(),
+                    TDSNhoNhat = new[] { cln.TDSDot1 , cln.TDSDot2 , cln.TDSDot3  }.Min(),
+                    TDSTrungBinh = Math.Round((double)new[] { cln.TDSDot1 , cln.TDSDot2 , cln.TDSDot3  }.Average()!, 2),
+                    DoCungLonNhat =new[] { cln.DoCungDot1 ?? 0, cln.DoCungDot2 ??0, cln.DoCungDot3 ?? 0 }.Max(),
                     DoCungNhoNhat = new[] { cln.DoCungDot1 ?? 0, cln.DoCungDot2 ?? 0, cln.DoCungDot3 ?? 0 }.Min(),
-                    DoCungTrungBinh = Math.Round((double)new[] { cln.DoCungDot1 ?? 0, cln.DoCungDot2 ?? 0, cln.DoCungDot3 ?? 0 }.Average()!, 2),
+                    DoCungTrungBinh = Math.Round((double)new[] { cln.DoCungDot1 ?? 0, cln.DoCungDot2 ??0, cln.DoCungDot3 ??0}.Average()!, 2),
                     ArsenicLonNhat = new[] { cln.ArsenicDot1 ?? 0, cln.ArsenicDot2 ?? 0, cln.ArsenicDot3 ?? 0 }.Max(),
                     ArsenicNhoNhat = new[] { cln.ArsenicDot1 ?? 0, cln.ArsenicDot2 ?? 0, cln.ArsenicDot3 ?? 0 }.Min(),
-                    ArsenicTrungBinh = Math.Round((double)new[] { cln.ArsenicDot1 ?? 0, cln.ArsenicDot2 ?? 0, cln.ArsenicDot3 ?? 0 }.Average()!, 2),
-                    ChlorideLonNhat = new[] { cln.ChlorideDot1 ?? 0, cln.ChlorideDot2 ?? 0, cln.ChlorideDot3 ?? 0 }.Max(),
-                    ChlorideNhoNhat = new[] { cln.ChlorideDot1 ?? 0, cln.ChlorideDot2 ?? 0, cln.ChlorideDot3 ?? 0 }.Min(),
-                    ChlorideTrungBinh = Math.Round((double)new[] { cln.ChlorideDot1 ?? 0, cln.ChlorideDot2 ?? 0, cln.ChlorideDot3 ?? 0 }.Average()!, 2),
+                    ArsenicTrungBinh =Math.Round((double)new[] { cln.ArsenicDot1 ?? 0, cln.ArsenicDot2 ?? 0, cln.ArsenicDot3 ?? 0 }.Average()!, 2),
+                    ChlorideLonNhat = new[] { cln.ChlorideDot1 , cln.ChlorideDot2 , cln.ChlorideDot3  }.Max(),
+                    ChlorideNhoNhat = new[] { cln.ChlorideDot1 , cln.ChlorideDot2 , cln.ChlorideDot3  }.Min(),
+                    ChlorideTrungBinh = Math.Round((double)new[] { cln.ChlorideDot1 , cln.ChlorideDot2 , cln.ChlorideDot3  }.Average()!, 2),
 
                 })
                 .OrderBy(d => d.Id)
